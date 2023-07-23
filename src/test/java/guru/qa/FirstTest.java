9fda43648baa2070a5eb6f685739fa97d5107003
Anna V <annreznik134@gmail.com>
2023-07-23 12:29:55 +0300

    junit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,16 @@
+package guru.qa;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.lang.module.Configuration;
+
+public class FirstTest {
+    @BeforeAll
+    static void beforeAll(){
+    }
+    @Test
+    void firstTest(){
+
+    }
+}
