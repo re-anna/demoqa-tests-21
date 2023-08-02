7b12af8e7a305c5659558a1210ace4faf0892e9a
Anna V <annreznik134@gmail.com>
2023-08-03 00:23:50 +0300

    Soft assertion page is open

--- src/test/java/guru/qa/java/com/demoqa/SoftAssertionTest.java
+++ src/test/java/guru/qa/java/com/demoqa/SoftAssertionTest.java
@@ -1,24 +1,16 @@
 package guru.qa.java.com.demoqa;
 
+
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-
-public class SoftAssertionTest {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-    }
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class SoftAssertionTest {
@@ -26,13 +18,35 @@ public class SoftAssertionTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
 
     @Test
-    void softAssertionsHasJUnit5(){
-        open("https://github.com/");
-        $("qbsearch-input.inputButtonText").click();
+    void softAssertionsHasJUnit5() {
 
-}
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(Condition.text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("Using JUnit5 extend test class:\n" +
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+
+        @AfterAll
+        static void afterAll () {
+            Selenide.closeWebDriver();
+        }
+    }
+
+
+
