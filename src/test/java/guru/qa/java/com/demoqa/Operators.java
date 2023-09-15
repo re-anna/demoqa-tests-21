4da15d42ff1f795d291a4911bfdfb4550519d258
Anna V <annreznik134@gmail.com>
2023-09-15 21:52:59 +0300

    Add info about operators

---
+++ src/test/java/guru/qa/java/com/demoqa/Operators.java
@@ -0,0 +1,54 @@
+package guru.qa.java.com.demoqa;
+
+public class Operators {
+    public static void main(String[] args) {
+        // Арифметические операторы
+        int a = 4;
+        int b = 3;
+        int c = 1;
+        System.out.println("Арифметические операторы");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("a++   = " + (++a));
+        System.out.println("b--   = " + (b--));
+        System.out.println("______________________________________________");
+
+
+        // Операторы сравнения
+        System.out.println("Операторы сравнени");
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a <= b = " + (a <= b));
+        System.out.println("______________________________________________");
+
+        // Логические операторы
+        boolean d = true;
+        boolean e = false;
+
+        System.out.println("Логические операторы");
+        System.out.println("d && e = " + (d && e));
+        System.out.println("d || e = " + (d || e));
+        System.out.println("!(d && e) = " + !(d && e));
+        System.out.println("______________________________________________");
+
+        // Переполнение
+        int f = Integer.MAX_VALUE;
+        System.out.println("Переполнение");
+        System.out.println("maximum Integer = " + f);
+        System.out.println("Integer overflow " + (f + 1));
+        System.out.println("______________________________________________");
+
+        // Комбинация типов
+        int g = 1;
+        double h = 5.53;
+        System.out.println("Комбинация типов");
+        System.out.println("int + double = " + (g + h));
+        System.out.println("int / double = " + (g / h));
+        System.out.println("______________________________________________");
+    }
+}
